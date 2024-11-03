4829a6bba5eb1281f00dda3ea8cd75fa0f860687
ManpreetSingh04335 <manpreet.04335@gmail.com>
2024-11-03 23:50:42 +0530

    Revert "Mannu modified settings.py"
    This reverts commit 4e964b4c726b35c2af02127a1b5c246dde4c77e9.

--- Barber/Barber/Barber/settings.py
+++ Barber/Barber/Barber/settings.py
@@ -81,17 +81,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'HImPath',
-#         'USER':'postgres',
-#         'PASSWORD':'4149',
-#         'HOST':'localhost',
-#         'PORT':'5432'
-#     }
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
