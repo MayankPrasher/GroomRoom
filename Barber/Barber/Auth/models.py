29b843d94ea558d7222167d15b42143cbb7a5de0
Manpreet Singh <166596061+ManpreetSingh04335@users.noreply.github.com>
2024-11-06 21:00:25 +0530

    Manpreet Singh modified created authorization and created the database. (#2)
    * Mannu modified readme.txt
    
    * Mannu modified settings.py
    
    * Revert "Mannu modified settings.py"
    
    This reverts commit 4e964b4c726b35c2af02127a1b5c246dde4c77e9.
    
    * Mannu modified settings.py
    
    * Mannu modified changed database to postgres.
    
    * Mannu modified created authorization and created the database.

--- Barber/Barber/Auth/models.py
+++ Barber/Barber/Auth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Barber(models.Model):
+    barber_code = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.barber_code
--- Barber/Barber/app/views.py
+++ Barber/Barber/app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def landing_view(request):
+    if not request.session.get('is_logged_in'):
+        return redirect('Auth')
     return render(request, 'Barber/landing.html', {})
-
