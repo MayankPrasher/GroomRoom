d2e4b44c5cfb7d84fcc08a9ea8013da3e23412b7
ManpreetSingh04335 <manpreet.04335@gmail.com>
2024-11-06 20:50:31 +0530

    Mannu modified created authorization and created the database.

--- Barber/Barber/Auth/views.py
+++ Barber/Barber/Auth/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Barber
 
 def home_view(request):
+    if request.method == 'POST':
+        barber_code = request.POST.get('barber_code')
+        password = request.POST.get('password')
+
+        try:
+            # isse ye barber_007 ko database se uthaega aur usse match krega
+            barber = Barber.objects.get(barber_code=barber_code)
+
+            # phir check krega password shi hai to redirect kr dega landing page pe
+            if barber.password == password:
+                request.session['is_logged_in'] = True
+                return redirect('landing')
+            else:
+                messages.error(request, 'Invalid Barber Code or Password')
+        
+        except Barber.DoesNotExist:
+            messages.error(request, 'Invalid Barber Code or Password')
     return render(request, 'Auth/auth.html', {})
--- Barber/Barber/app/urls.py
+++ Barber/Barber/app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_view, name="landing"), 
+    path('', views.landing_view, name="landing"),
 ]
