4f3e862aef906a6ac938f8d1f6e42a4c36ada5d1
Manpreet Singh <166596061+ManpreetSingh04335@users.noreply.github.com>
2024-11-16 20:39:42 +0530

    Mannu Logout Functionality done (#5)

--- Barber/Barber/app/urls.py
+++ Barber/Barber/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_view, name="landing"),
+    path('logout/', views.logout_view, name="logout"),
 ]
--- Barber/Barber/app/views.py
+++ Barber/Barber/app/views.py
@@ -4,3 +4,8 @@ def landing_view(request):
     if not request.session.get('is_logged_in'):
         return redirect('Auth')
     return render(request, 'Barber/landing.html', {})
+
+def logout_view(request):
+    if 'is_logged_in' in request.session:
+        del request.session['is_logged_in']
+    return redirect('Auth')
