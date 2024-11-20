b31b2d2a23c3d5999cdb9478562564ad6211334c
Mayank Prasher <prasher6789@gmail.com>
2024-11-20 15:32:06 +0530

    added seat functionality and inflate the left sidebar

--- Barber/Barber/app/views.py
+++ Barber/Barber/app/views.py
@@ -5,12 +5,12 @@ def landing_view(request):
         return redirect('Auth')
     return render(request, 'Barber/landing.html', {})
 
-<<<<<<< HEAD
+
 def logout_view(request):
     if 'is_logged_in' in request.session:
         del request.session['is_logged_in']
     return redirect('Auth')
-=======
+
 def logout_view(request): 
     if request.method == "POST":
         if 'is_logged_in' in request.session:
@@ -18,4 +18,4 @@ def logout_view(request):
         return redirect('Auth')
     else:
         return render(request, 'Barber/landing.html', {})
->>>>>>> 05c7ad8ea70e1b60d4d30b721df8009494248594
+
