d54d7766bd28b8d7ccf5184859d97ead9214607c
ManpreetSingh04335 <manpreet.04335@gmail.com>
2024-11-24 00:15:03 +0530

    Mannu created the user project and database for user

--- User/User/User/settings.py
+++ User/User/User/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'UserApp',
+    'UserAuth',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'User.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GroomRoom',
+        'USER':'postgres',
+        'PASSWORD':'4149',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
